128c5cbea311503930897d32ac9cb95b663ad58e
Laura Natalia González Mora <lngonzalezm@unal.edu.co>
2020-10-05 17:25:14 -0500

    Set the loggings and change the port
    In application.properties you must change the directory for the file of the loggs, the new port is 8008

--- src/main/java/com/hips/api/ApiApplication.java
+++ src/main/java/com/hips/api/ApiApplication.java
@@ -1,25 +1,16 @@
 package com.hips.api;
 
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.*;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.context.annotation.Bean;
 
 import com.hips.api.models.*;
-import com.hips.api.repositories.UserAccountRepository;
+//import com.hips.api.repositories.UserAccountRepository;
 
-@RestController
-@EnableAutoConfiguration
-@EnableJpaAuditing
+
 @SpringBootApplication
 public class ApiApplication {
 
-	@RequestMapping("/")
-	String home() {
-		return "Hello World!";
-	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
