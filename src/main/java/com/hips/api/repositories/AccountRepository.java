42f4434912e5e5b1eeb4c6bb860191b015694108
jhonatan torres <jtorresf@unal.edu.co>
2020-10-14 00:09:24 -0500

    recover account repository+

---
+++ src/main/java/com/hips/api/repositories/AccountRepository.java
@@ -0,0 +1,13 @@
+package com.hips.api.repositories;
+
+import com.hips.api.models.Account;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+@RepositoryRestResource
+public interface AccountRepository extends CrudRepository<Account, Integer>{
+
+    Account findByEmail(String email);
+}
