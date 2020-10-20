f693de33fadf5e3d599f34f74816c90773b78e02
dgonzalezo <dgonzalezo@unal.edu.co>
2020-10-19 23:06:08 -0500

    resolve conflicts login and setgoal

--- src/main/java/com/hips/api/repositories/AccountRepository.java
+++ src/main/java/com/hips/api/repositories/AccountRepository.java
@@ -1,15 +1,11 @@
 package com.hips.api.repositories;
 
 import com.hips.api.models.Account;
-<<<<<<< HEAD
 import com.hips.api.models.UserAccount;
-=======
->>>>>>> feature/lngonzalezm/login
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
-<<<<<<< HEAD
 import java.util.Optional;
 
 @RepositoryRestResource
@@ -18,10 +14,4 @@ public interface AccountRepository extends CrudRepository<Account, Integer>{
     Account findByEmail(String email);
     Account findById(int userId);
 }
-=======
 
-@RepositoryRestResource
-public interface AccountRepository extends CrudRepository<Account, Integer> {
-    Account findByEmail(String email);
-}
->>>>>>> feature/lngonzalezm/login
