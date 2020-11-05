0c04778bf9949d86b37a36c3bf7d6e6fe18ec090
Cristian García <ccgarciab@unal.edu.co>
2020-11-04 20:19:53 -0500

    Created Exercise Datapoint model.

---
+++ src/main/java/com/hips/api/models/ExerciseDataPoint.java
@@ -0,0 +1,50 @@
+package com.hips.api.models;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+public class ExerciseDataPoint extends Auditable implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(columnDefinition = "serial")
+    int id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    PlannedExercise plannedExercise;
+
+    Date date;
+
+    public ExerciseDataPoint() {
+    }
+
+    public ExerciseDataPoint(PlannedExercise plannedExercise, Date date) {
+        this.plannedExercise = plannedExercise;
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public PlannedExercise getPlannedExercise() {
+        return plannedExercise;
+    }
+
+    public void setPlannedExercise(PlannedExercise plannedExercise) {
+        this.plannedExercise = plannedExercise;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
